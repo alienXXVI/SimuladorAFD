d99d0a93c6ffc13445c4aa286aebbdb6d2c3ae2a
alienXXVI <aliana.wkssg@gmail.com>
2024-02-19 10:00:07 -0300

    leitura de arquivo

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include "sistema.h"
+#include "arquivo.h"
+
 
 // bacca
 int main() {
-    int estados[] = {0, 1, 2, -1};
-    char alfabeto[] = {'a', 'b', 'c'};
-    int finais[] = {2, -1};
-    GRAMATICA *gramatica = criar_gramatica(alfabeto, estados, finais);
+    // int estados[] = {0, 1, 2, -1};
+    // char alfabeto[] = {'a', 'b', 'c'};
+    // int finais[] = {2, -1};
+    GRAMATICA *gramatica = NULL;
 
-    inserir_transicao(gramatica, 0, 2, 'a');
-    inserir_transicao(gramatica, 0, 0, 'b');
-    inserir_transicao(gramatica, 1, 2, 'c');
-    inserir_transicao(gramatica, 1, 0, 'a');
-    inserir_transicao(gramatica, 2, 2, 'c');
-    inserir_transicao(gramatica, 2, 1, 'a');
-    inserir_transicao(gramatica, 2, 0, 'b');
+    // inserir_transicao(gramatica, 0, 2, 'a');
+    // inserir_transicao(gramatica, 0, 0, 'b');
+    // inserir_transicao(gramatica, 1, 2, 'c');
+    // inserir_transicao(gramatica, 1, 0, 'a');
+    // inserir_transicao(gramatica, 2, 2, 'c');
+    // inserir_transicao(gramatica, 2, 1, 'a');
+    // inserir_transicao(gramatica, 2, 0, 'b');
 
+    // imprimir_gramatica(gramatica);
+
+    // testar_palavra(gramatica, "bacca");
+
+    gramatica = formar_gramatica("afd.txt");
     imprimir_gramatica(gramatica);
+    testar_palavra()
 
-    testar_palavra(gramatica, "bacca");
+    return 0;
 }
